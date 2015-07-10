d58bccd161dec5df91dbdd6b2b5a125d083dacc1
leoYY <yuan.yi.coding@gmail.com>
2015-07-10 16:29:37 +0800

    add part code

--- src/flags.cc
+++ src/flags.cc
@@ -48,5 +48,6 @@ DEFINE_int32(agent_app_stop_wait_retry_times, 10, "how many times for stop wait"
 DEFINE_string(monitor_conf_path, "", "path of monitor conf");
 
 DEFINE_string(pam_pwd_dir, "/tmp/", "directory that stores galaxy-ssh passwords on agent node");
+DEFINE_int32(agent_stop_timeout, 3, "agent stop user command delay timeout, unit s");
 
 /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
