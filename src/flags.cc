0ee1d0f367adf9d896d1c71b75a21da3dec5a3d5
leoYY <yuan.yi.coding@gmail.com>
2015-08-10 14:55:13 +0800

    update test

--- src/flags.cc
+++ src/flags.cc
@@ -37,7 +37,7 @@ DEFINE_int32(agent_rpc_initd_timeout, 2, "agent monitor initd interval, unit sec
 // gce
 DEFINE_string(gce_cgroup_root, "/cgroups/", "Cgroup root mount path");
 DEFINE_string(gce_support_subsystems, "cpu,memory,cpuacct,frozen", "Cgroup default support subsystems");
-DEFINE_int64(gce_initd_zombie_check_interval, 10000, "Initd Zombie Check Interval");
+DEFINE_int64(gce_initd_zombie_check_interval, 100, "Initd Zombie Check Interval");
 DEFINE_string(gce_initd_dump_file, "initd_checkpoint_file", "Initd Checkpoint File Name");
 DEFINE_string(gce_initd_port, "8765", "gce initd listen port");
 DEFINE_string(gce_gced_port, "8766", "gce initd listen port");
