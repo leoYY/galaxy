0ab4fa55f5c9cdd7fc7324fcddaa03bb18e81d42
leoYY <yuan.yi.coding@gmail.com>
2015-08-13 17:07:51 +0800

    merge trunk and update sandbox

--- sandbox/stop_all.sh
+++ sandbox/stop_all.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
-killall -9 ins
-killall -9 master
-killall -9 scheduler
-killall -9 agent
-killall -9 gced
-killall -9 initd
+killall ins
+killall master
+killall scheduler
+killall agent
